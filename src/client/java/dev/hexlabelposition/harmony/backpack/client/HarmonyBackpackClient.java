f3dfd68187b767349f7a1b7c1c8e3ff237d1b78b
Vladyslav Kudriashov <hexlabelposition@gmail.com>
2026-09-02 00:31:02 +0200

    init: create Fabric mod using template generator

---
+++ src/client/java/dev/hexlabelposition/harmony/backpack/client/HarmonyBackpackClient.java
@@ -0,0 +1,10 @@
+package dev.hexlabelposition.harmony.backpack.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HarmonyBackpackClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
